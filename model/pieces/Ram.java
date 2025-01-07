c396b62b51b9141760c8ad994aa70d8fb6093ae5
Harris Majeed <harrismajeed2004@gmail.com>
2025-01-08 04:48:56 +0800

    add Ram

---
+++ model/pieces/Ram.java
@@ -0,0 +1,58 @@
+package model.pieces;
+
+import java.util.LinkedList;
+import java.util.Optional;
+
+import model.BoardVerticalEdgeListener;
+import model.CellPosition;
+import model.Player;
+
+public class Ram extends Piece implements BoardVerticalEdgeListener
+{
+    private boolean facingUp;
+
+    public Ram(Player player, boolean facingUp)
+    {
+        super(player);
+        this.facingUp = facingUp;
+    }
+
+    public boolean isFacingUp()
+    {
+        return facingUp;
+    }
+
+    @Override
+    public boolean canJump()
+    {
+        return false;
+    }
+
+    @Override
+    public Optional<LinkedList<CellPosition>> getPotentialPath(
+            CellPosition fromCellPos, CellPosition toCellPos)
+    {
+        if (fromCellPos.column() != toCellPos.column())
+        {
+            return Optional.empty();
+        }
+
+        int rowDifference = Math.abs(fromCellPos.row() - toCellPos.row());
+
+        if (rowDifference == 1)
+        {
+            LinkedList<CellPosition> path = new LinkedList<>();
+            path.add(toCellPos);
+            return Optional.of(path);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public void onBoardVerticalEdgeReached()
+    {
+        facingUp = !facingUp;
+    }
+
+}
