0c0ff88f42933f7412a489b9cde9af17f6d1a460
Harris Majeed <harrismajeed2004@gmail.com>
2025-01-09 00:53:25 +0800

    implement Tor piece

---
+++ model/pieces/Tor.java
@@ -0,0 +1,51 @@
+package model.pieces;
+
+import java.util.LinkedList;
+import java.util.Optional;
+
+import model.CellPosition;
+import model.Player;
+
+public class Tor extends Piece
+{
+    public Tor(Player player)
+    {
+        super(player);
+    }
+
+    @Override
+    public boolean canJump()
+    {
+        return false;
+    }
+
+    @Override
+    public Optional<LinkedList<CellPosition>> getPotentialPath(
+            CellPosition fromCellPos, CellPosition toCellPos)
+    {
+        int rowDifference = toCellPos.row() - fromCellPos.row();
+        int columnDifference = toCellPos.column() - fromCellPos.column();
+
+        if (rowDifference != 0 && columnDifference != 0)
+        {
+            return Optional.empty();
+        }
+
+        LinkedList<CellPosition> path = new LinkedList<>();
+        int rowStep = Integer.signum(rowDifference);
+        int columnStep = Integer.signum(columnDifference);
+
+        int row = fromCellPos.row();
+        int column = fromCellPos.column();
+
+        while (row != toCellPos.row() || column != toCellPos.column())
+        {
+            row += rowStep;
+            column += columnStep;
+            path.add(new CellPosition(row, column));
+        }
+
+        return Optional.of(path);
+    }
+
+}
