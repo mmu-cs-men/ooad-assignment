64e453614272f1bc54652748988b00c6ed89fc15
Harris Majeed <harrismajeed2004@gmail.com>
2025-01-09 01:38:03 +0800

    add KwazamBoard

---
+++ model/boards/KwazamBoard.java
@@ -0,0 +1,69 @@
+package model.boards;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Cell;
+import model.Player;
+import model.pieces.Biz;
+import model.pieces.Ram;
+import model.pieces.Sau;
+import model.pieces.Tor;
+import model.pieces.Xor;
+
+public class KwazamBoard extends Board
+{
+    public KwazamBoard(List<Player> players)
+    {
+        super(players);
+    }
+
+    @Override
+    protected ArrayList<ArrayList<Cell>> populateCells()
+    {
+        ArrayList<ArrayList<Cell>> cells = new ArrayList<>();
+        for (int row = 0; row < getBoardRows(); row++)
+        {
+            ArrayList<Cell> rowCells = new ArrayList<>();
+            for (int column = 0; column < getBoardColumns(); column++)
+            {
+                rowCells.add(new Cell());
+            }
+            cells.add(rowCells);
+        }
+
+        cells.get(0).get(0).setPiece(new Tor(players.get(0)));
+        cells.get(0).get(1).setPiece(new Biz(players.get(0)));
+        cells.get(0).get(2).setPiece(new Sau(players.get(0)));
+        cells.get(0).get(3).setPiece(new Biz(players.get(0)));
+        cells.get(0).get(4).setPiece(new Xor(players.get(0)));
+        for (int column = 0; column < getBoardColumns(); column++)
+        {
+            cells.get(1).get(column).setPiece(new Ram(players.get(0), false));
+        }
+
+        for (int column = 0; column < getBoardColumns(); column++)
+        {
+            cells.get(6).get(column).setPiece(new Ram(players.get(0), true));
+        }
+        cells.get(7).get(0).setPiece(new Xor(players.get(0)));
+        cells.get(7).get(1).setPiece(new Biz(players.get(0)));
+        cells.get(7).get(2).setPiece(new Sau(players.get(0)));
+        cells.get(7).get(3).setPiece(new Biz(players.get(0)));
+        cells.get(7).get(4).setPiece(new Tor(players.get(0)));
+
+        return cells;
+    }
+
+    @Override
+    protected int getBoardRows()
+    {
+        return 8;
+    }
+
+    @Override
+    protected int getBoardColumns()
+    {
+        return 5;
+    }
+}
