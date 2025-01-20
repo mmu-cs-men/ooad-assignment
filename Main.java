c6434a170d09492672eecee88f6a9da4c5e98d61
Harris Majeed <harrismajeed2004@gmail.com>
2025-01-21 06:17:43 +0800

    centralize main method

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import controller.GameController;
+import view.KwazamGUI;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        KwazamGUI gui = new KwazamGUI();
+        new GameController(gui);
     }
 }
