e9b5a3386ddd89f6dadc780538423aff11ade1ef
Abdullah Hawash <abdullahhawash93@gmail.com>
2025-01-24 04:06:20 +0800

    Remove unused imports

--- view/KwazamGUI.java
+++ view/KwazamGUI.java
@@ -1,8 +1,6 @@
 package view;
 
 import java.awt.image.BufferedImage;
-import model.game.Player;
-import model.listeners.WinListener;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
