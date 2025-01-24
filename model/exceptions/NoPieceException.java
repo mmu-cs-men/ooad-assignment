39f5d2c1d199f4f6c9609ef371c37f9e850e6bce
Harris Majeed <harrismajeed2004@gmail.com>
2025-01-24 17:27:54 +0800

    add comments to exceptions
    * NoPieceException
    * NotYourPieceException
    * PieceMoveException

--- model/exceptions/NoPieceException.java
+++ model/exceptions/NoPieceException.java
@@ -1,5 +1,14 @@
 package model.exceptions;
 
+/**
+ * This exception is thrown when there is an attempt to access a piece in a cell
+ * that does not contain any piece on the game board.
+ * <p>
+ * Use this if you need a piece to exist before performing some action like
+ * moving.
+ *
+ * @author Harris Majeed
+ */
 public class NoPieceException extends RuntimeException
 {
     public NoPieceException()
--- model/exceptions/PieceMoveException.java
+++ model/exceptions/PieceMoveException.java
@@ -1,5 +1,16 @@
 package model.exceptions;
 
+/**
+ * This exception is thrown when there is attempt to move a piece in an
+ * "invalid" way.
+ * <p>
+ * In Kwazam Chess, this exception is thrown when the player attempts to move a
+ * piece in a way that does not follow its movement pattern (e.g. moving a Ram
+ * two cells ahead). This exception is also thrown if a piece that can't jump
+ * tries to move to a cell that is obstructed.
+ *
+ * @author Harris Majeed
+ */
 public class PieceMoveException extends RuntimeException
 {
     public PieceMoveException()
