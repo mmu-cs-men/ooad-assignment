6579c968175936f621296b3938f725e1e8945402
Harris Majeed <harrismajeed2004@gmail.com>
2025-01-24 19:47:11 +0800

    add comments to KwazamGameMaster

--- model/game/KwazamGameMaster.java
+++ model/game/KwazamGameMaster.java
@@ -3,13 +3,30 @@ package model.game;
 import model.board.KwazamBoard;
 import utils.CircularLinkedList;
 
+/**
+ * Concrete implementation of {@code GameMaster} for Kwazam Chess.
+ *
+ * @author Harris Majeed
+ * @see GameMaster
+ * @see KwazamBoard
+ */
 public class KwazamGameMaster extends GameMaster<KwazamBoard>
 {
+    /**
+     * Creates a new KwazamGameMaster with the given board and list of players.
+     *
+     * @param board   the board for this game
+     * @param players the list of players participating in this game
+     */
     public KwazamGameMaster(KwazamBoard board, CircularLinkedList<Player> players)
     {
         super(board, players);
     }
 
+    /**
+     * Advances the turn to the next player and switches Tor/Xor every second
+     * turn according to the assignment specification.
+     */
     @Override
     public void advanceTurn()
     {
