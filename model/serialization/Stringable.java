e515080d3cb64afeff1fa218708d5ef15f7dc29e
Abdullah Hawash <abdullahhawash93@gmail.com>
2025-01-26 23:24:29 +0800

    Add JavaDoc comment to Stringable interface

--- model/serialization/Stringable.java
+++ model/serialization/Stringable.java
@@ -1,5 +1,10 @@
 package model.serialization;
 
+
+/**
+ * Defines a contract for retrieving a string-based representation of an object.
+ * @author Harris Majeed
+ */
 public interface Stringable
 {
     String getStringRepresentation();
